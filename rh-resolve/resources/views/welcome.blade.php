380eaae02818de4c680370dfbdd648622fb9f490
max <mxmascarenhasz7@gmail.com>
2024-09-14 15:53:41 -0300

    Altura fixa no chat

--- rh-resolve/resources/views/welcome.blade.php
+++ rh-resolve/resources/views/welcome.blade.php
@@ -14,59 +14,6 @@
 </head>
 
 <body>
-
-    <nav class="navbar" role="navigation" aria-label="main navigation">
-        <div class="navbar-brand">
-            <h4 id="logo" class="roboto-bold">RH RESOLVE+</h4>
-            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-            </a>
-        </div>
-
-        <div id="navbarBasicExample" class="navbar-menu">
-            <div class="navbar-start">
-                <a class="navbar-item roboto-regular">Início</a>
-                <a class="navbar-item roboto-regular">ChatBot</a>
-            </div>
-
-            <div class="navbar-end">
-                <div class="navbar-item">
-                    <div class="buttons">
-                        <a id="pfp" class="button is-primary">
-                            <img src="img/account_circle_50dp_E8EAED_FILL0_wght400_GRAD0_opsz48.svg" alt="Profile Picture">
-                        </a>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-    </nav>
-
-    <section class="section">
-        <div class="container">
-            <div class="columns">
-                <div class="column is-half is-offset-0">
-                    <h1 class="roboto-black">Mural<br>Informativo</h1>
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <footer class="footer">
-        <div class="content has-text-centered">
-            <p>
-                <strong>DEV. Sistemas | 4° MOD.</strong> by <a href="https://www.instagram.com/maquessssss">Max</a>,<a href="https://www.instagram.com/ian_filgueiras"> Ian</a>,<a href="https://www.instagram.com/leoosilva9_"> Leonardo</a>.
-                The source code is licensed
-                <a href="https://opensource.org/license/mit">MIT</a>. The
-                website content is licensed
-                <a href="https://creativecommons.org/licenses/by-nc-sa/4.0//">CC BY NC SA 4.0</a>.
-            </p>
-        </div>
-    </footer>
-
     <script src="{{ asset('js/chatbot.js') }}"></script>
 </body>
 
