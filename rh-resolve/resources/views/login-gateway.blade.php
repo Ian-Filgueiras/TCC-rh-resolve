36b5e65cf95f3c60be51215aa82c1151b6338909
max <mxmascarenhasz7@gmail.com>
2024-09-15 02:00:11 -0300

    Responsividade ajustada

--- rh-resolve/resources/views/login-gateway.blade.php
+++ rh-resolve/resources/views/login-gateway.blade.php
@@ -9,7 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.4/css/bulma.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <title>RH Resolve+ | Solução Administrativa</title>
+    <title>RH Resolve+</title>
 </head>
 
 <body>
@@ -28,7 +28,7 @@
     </div>
   </div>
   <div class="call-text">
-    <h1><span>RH Resolve+</span> Praticidade e clareza no seu suporte.</h1>
+    <h1><span>RH Resolve+</span> Praticidade e clareza no seu suporte, como essa tela.</h1>
     <button class="btn btn-success">Vamos Começar!</button>
   </div>
 </div>
