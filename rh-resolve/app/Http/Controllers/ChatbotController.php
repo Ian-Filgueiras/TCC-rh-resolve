8694df447d9de650dc24a4d00e943926a041a891
ian <iansantosfilgueiras@hotmail.com>
2024-09-15 11:02:02 -0300

    holerite

--- rh-resolve/app/Http/Controllers/ChatbotController.php
+++ rh-resolve/app/Http/Controllers/ChatbotController.php
@@ -15,11 +15,23 @@ class ChatbotController extends Controller
     {
         $message = $request->input('message');
         $conversationState = $request->session()->get('conversationState', 'main_menu');
+        $selectedTopic = $request->session()->get('selectedTopic', null);
 
-        $response = $this->processMessage($message, $conversationState);
+        // Se um tópico foi selecionado, processa as perguntas específicas do tópico
+        if ($selectedTopic) {
+            $response = $this->handleQuestionSelection($message, $selectedTopic);
+        } else {
+            // Caso contrário, continua o fluxo normal com base na escolha da mensagem inicial
+            $response = $this->processMessage($message, $conversationState);
+        }
 
-        // Atualiza o estado da conversa
+        // Atualiza o estado da conversa e o tópico selecionado
         $request->session()->put('conversationState', $response['state']);
+        if (isset($response['selectedTopic'])) {
+            $request->session()->put('selectedTopic', $response['selectedTopic']);
+        } else {
+            $request->session()->forget('selectedTopic');
+        }
 
         return response()->json(['response' => $response['message']]);
     }
@@ -44,24 +56,24 @@ class ChatbotController extends Controller
     private function handleMainMenu($message)
     {
         switch ($message) {
-            case '1':
+            case '1': // Início com informações padrões automáticas
                 return [
                     'message' => 'Aqui estão as informações automáticas padrão...',
                     'state' => 'main_menu'
                 ];
-            case '2':
+            case '2': // Perguntas pré-definidas para funcionários
                 return [
-                    'message' => 'Selecione a opção:<br>1. Holerite<br>2. Benefícios<br>3. Descontos<br>4. Horas Extras<br>5. Férias<br>6. Atestados Médicos<br>7. Direitos e Deveres da Empresa<br>8. Direitos e Deveres do Funcionário<br>9. Código de Conduta e Ética<br>10. Finalizar atendimento',
+                    'message' => 'Selecione um tópico:<br>1. Holerite<br>2. Benefícios<br>3. Descontos<br>4. Horas Extras<br>5. Férias<br>6. Atestados Médicos<br>7. Direitos e Deveres da Empresa<br>8. Direitos e Deveres do Funcionário<br>9. Código de Conduta e Ética',
                     'state' => 'pre_defined_questions'
                 ];
-            case '3':
+            case '3': // Falar com atendente
                 return [
-                    'message' => 'Entrando em contato com o atendente...',
+                    'message' => 'Entrando em contato com o atendente. Por favor, aguarde...',
                     'state' => 'contacting_attendant'
                 ];
-            case '4':
+            case '4': // Agendamento de horário presencial
                 return [
-                    'message' => 'Para agendar um horário presencial, por favor, informe a data desejada.',
+                    'message' => 'Para agendar um horário presencial, por favor, informe a data desejada ou acesse o calendário disponível no portal.',
                     'state' => 'scheduling'
                 ];
             default:
@@ -75,60 +87,69 @@ class ChatbotController extends Controller
     private function handlePreDefinedQuestions($message)
     {
         switch ($message) {
-            case '1':
-                return [
-                    'message' => 'Informações sobre Holerite...',
-                    'state' => 'main_menu'
-                ];
-            case '2':
-                return [
-                    'message' => 'Informações sobre Benefícios...',
-                    'state' => 'main_menu'
-                ];
-            case '3':
-                return [
-                    'message' => 'Informações sobre Descontos...',
-                    'state' => 'main_menu'
-                ];
-            case '4':
-                return [
-                    'message' => 'Informações sobre Horas Extras...',
-                    'state' => 'main_menu'
-                ];
-            case '5':
-                return [
-                    'message' => 'Informações sobre Férias...',
-                    'state' => 'main_menu'
-                ];
-            case '6':
-                return [
-                    'message' => 'Informações sobre Atestados Médicos...',
-                    'state' => 'main_menu'
-                ];
-            case '7':
-                return [
-                    'message' => 'Informações sobre Direitos e Deveres da Empresa...',
-                    'state' => 'main_menu'
-                ];
-            case '8':
-                return [
-                    'message' => 'Informações sobre Direitos e Deveres do Funcionário...',
-                    'state' => 'main_menu'
-                ];
-            case '9':
-                return [
-                    'message' => 'Informações sobre Código de Conduta e Ética...',
-                    'state' => 'main_menu'
-                ];
-            case '10':
-                return [
-                    'message' => 'Finalizando atendimento...',
-                    'state' => 'start' // ou um estado que indique o final ou retorno ao início
-                ];
+            case '1': // Holerite
+                return $this->getHoleriteQuestions();
             default:
                 return [
                     'message' => 'Opção inválida. Por favor, selecione uma opção válida.',
                     'state' => 'pre_defined_questions'
+                ];
+        }
+    }
+
+    // Função para apresentar as perguntas de Holerite
+    private function getHoleriteQuestions()
+    {
+        return [
+            'message' => 'Escolha uma pergunta sobre Holerite:<br>1. Quais são os principais componentes da folha de pagamento?<br>2. O que significa cada código/sigla na minha folha de pagamento?<br>3. Como posso solicitar uma correção ao identificar um erro na minha folha de pagamento?<br>4. Como são calculadas as contribuições na minha folha de pagamento?<br>5. Como posso verificar a conformidade da minha folha de pagamento com o meu contrato?',
+            'state' => 'holerite_questions',
+            'selectedTopic' => 'holerite'
+        ];
+    }
+
+    // Função para lidar com a escolha das perguntas de Holerite
+    private function handleQuestionSelection($message, $selectedTopic)
+    {
+        if ($selectedTopic == 'holerite') {
+            return $this->getHoleriteAnswer($message);
+        }
+
+        // Adicione outros tópicos aqui quando necessário
+    }
+
+    private function getHoleriteAnswer($message)
+    {
+        switch ($message) {
+            case '1':
+                return [
+                    'message' => 'Principais componentes da folha de pagamento:<br>- Salário Base: R$1.420,00<br>- FGTS: 8%<br>- Horas Extras: 50%<br>- Comissões: 5% do valor/ produto.',
+                    'state' => 'main_menu'
+                ];
+            case '2':
+                return [
+                    'message' => 'Códigos/Siglas:<br>- SAL: Salário base.<br>- FGTS: Fundo de Garantia do Tempo de Serviço.<br>- INSS: Instituto Nacional do Seguro Social.<br>- VT: Vale Transporte.<br>- VA: Vale Alimentação.',
+                    'state' => 'main_menu'
+                ];
+            case '3':
+                return [
+                    'message' => 'Para solicitar correção na folha de pagamento, entre em contato com um analista em tempo real ou agende um horário no RH.',
+                    'state' => 'main_menu'
+                ];
+            case '4':
+                return [
+                    'message' => 'Cálculo de contribuições:<br>- INSS: 9%<br>- FGTS: 8%<br>- Vale Transporte: 6%',
+                    'state' => 'main_menu'
+                ];
+            case '5':
+                return [
+                    'message' => 'Você pode verificar a conformidade da sua folha de pagamento com o seu contrato entrando em contato com um de nossos analistas.',
+                    'state' => 'main_menu'
+                ];
+            default:
+                return [
+                    'message' => 'Opção inválida. Por favor, selecione uma opção válida.',
+                    'state' => 'holerite_questions',
+                    'selectedTopic' => 'holerite'
                 ];
         }
     }
