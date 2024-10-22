fcff4d54b4b4f57f46006d3ac23b84768b84f1fb
ian <iansantosfilgueiras@hotmail.com>
2024-10-22 07:35:10 -0300

    auth

--- rh-resolve/routes/web.php
+++ rh-resolve/routes/web.php
@@ -1,17 +1,26 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChatbotController;
+use App\Http\Middleware\EnsureUserIsLoggedIn;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', function () {
-    return view('login-gateway');
+// Rota para exibir o formulário de login
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+
+// Rota para processar o formulário de login
+Route::post('/login', [LoginController::class, 'login']);
+
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+
+Route::middleware([EnsureUserIsLoggedIn::class])->group(function () {
+
+    Route::get('/chatbot', [ChatbotController::class, 'index'])->name('chatbot');;
+    Route::post('/chatbot', [ChatbotController::class, 'handle']);
+
 });
-
-
-
-Route::get('/chatbot', [ChatbotController::class, 'index'])->name('chatbot');;
-Route::post('/chatbot', [ChatbotController::class, 'handle']);
